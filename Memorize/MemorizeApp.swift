d9dd63bc1266473a690dc70c92a5cc921c485cc9
So Mei Amy Lam <lamsomeiamy@gmail.com>
2024-02-28 18:42:22 -0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Amy Lam on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
